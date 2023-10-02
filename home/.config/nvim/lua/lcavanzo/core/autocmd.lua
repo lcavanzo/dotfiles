fe237e7a6487759b91f96f25542e55cb7bb2cef1
luis cavanzo <cavanzo1996@gmail.com>
2023-10-02 14:52:03 -0600

    Update autocmd.lua

--- home/.config/nvim/lua/lcavanzo/core/autocmd.lua
+++ home/.config/nvim/lua/lcavanzo/core/autocmd.lua
@@ -1,24 +1,68 @@
-vim.cmd("autocmd!")
+local api = vim.api
 
--- Leader
-vim.cmd('let mapleader=" "')
-
--- cmd
-vim.cmd("set termguicolors")
-vim.cmd("set hidden")
-vim.cmd("set noswapfile")
-vim.cmd("set noerrorbells")
-
--- Folds
-vim.cmd("set foldmethod=syntax") -- or indent
-vim.cmd("set foldlevelstart=99")
-
--- Undercurl
-vim.cmd([[let &t_Cs = "\e[4:3m"]])
-vim.cmd([[let &t_Ce = "\e[4:0m"]])
-
--- Turn off paste mode when leaving insert
-vim.api.nvim_create_autocmd("InsertLeave", {
-  pattern = '*',
-  command = "set nopaste"
+--- Remove all trailing whitespace on save
+local TrimWhiteSpaceGrp = api.nvim_create_augroup("TrimWhiteSpaceGrp", { clear = true })
+api.nvim_create_autocmd("BufWritePre", {
+  command = [[:%s/\s\+$//e]],
+  group = TrimWhiteSpaceGrp,
 })
+
+-- don't auto comment new line
+api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
+
+
+-- detect typst filetype
+api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.typ" },
+  callback = function()
+    vim.api.nvim_command("set filetype=typst")
+  end,
+})
+
+
+-- Highlight on yank
+api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+
+-- go to last loc when opening a buffer
+api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+
+-- show cursor line only in active window
+local cursorGrp = api.nvim_create_augroup("CursorLine", { clear = true })
+api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+  pattern = "*",
+  command = "set cursorline",
+  group = cursorGrp,
+})
+api.nvim_create_autocmd(
+  { "InsertEnter", "WinLeave" },
+  { pattern = "*", command = "set nocursorline", group = cursorGrp }
+)
+
+
+-- Enable spell checking for certain file types
+api.nvim_create_autocmd(
+  { "BufRead", "BufNewFile" },
+  -- { pattern = { "*.txt", "*.md", "*.tex" }, command = [[setlocal spell<cr> setlocal spelllang=en,de<cr>]] }
+  {
+    pattern = { "*.txt", "*.md", "*.tex" },
+    callback = function()
+      vim.opt.spell = true
+      vim.opt.spelllang = "en,de"
+    end,
+  }
+)
+
