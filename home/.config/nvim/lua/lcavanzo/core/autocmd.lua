8a06176a4d776aa8b51a5b2e503dad81c9484f89
lcavanzo <cavanzo1996@gmail.com>
2023-10-10 21:49:22 -0600

    update keymaps

--- home/.config/nvim/lua/lcavanzo/core/autocmd.lua
+++ home/.config/nvim/lua/lcavanzo/core/autocmd.lua
@@ -1,75 +1,93 @@
 local api = vim.api
 
---- Remove all trailing whitespace on save
+-- This file is automatically loaded by lazyvim.config.init.
+local function augroup(name)
+	return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- Check if we need to reload the file when it changed
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+	group = augroup("checktime"),
+	command = "checktime",
+})
+
 local TrimWhiteSpaceGrp = api.nvim_create_augroup("TrimWhiteSpaceGrp", { clear = true })
 api.nvim_create_autocmd("BufWritePre", {
-  command = [[:%s/\s\+$//e]],
-  group = TrimWhiteSpaceGrp,
+	command = [[:%s/\s\+$//e]],
+	group = TrimWhiteSpaceGrp,
 })
 
 -- don't auto comment new line
 api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
 
-
 -- detect typst filetype
 api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = { "*.typ" },
-  callback = function()
-    vim.api.nvim_command("set filetype=typst")
-  end,
+	pattern = { "*.typ" },
+	callback = function()
+		vim.api.nvim_command("set filetype=typst")
+	end,
 })
-
 
 -- Highlight on yank
 api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
-
 
 -- go to last loc when opening a buffer
 api.nvim_create_autocmd("BufReadPost", {
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
-  end,
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
 })
-
 
 -- show cursor line only in active window
 local cursorGrp = api.nvim_create_augroup("CursorLine", { clear = true })
 api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
-  pattern = "*",
-  command = "set cursorline",
-  group = cursorGrp,
+	pattern = "*",
+	command = "set cursorline",
+	group = cursorGrp,
 })
 api.nvim_create_autocmd(
-  { "InsertEnter", "WinLeave" },
-  { pattern = "*", command = "set nocursorline", group = cursorGrp }
+	{ "InsertEnter", "WinLeave" },
+	{ pattern = "*", command = "set nocursorline", group = cursorGrp }
 )
-
 
 -- Enable spell checking for certain file types
 api.nvim_create_autocmd(
-  { "BufRead", "BufNewFile" },
-  -- { pattern = { "*.txt", "*.md", "*.tex" }, command = [[setlocal spell<cr> setlocal spelllang=en,de<cr>]] }
-  {
-    pattern = { "*.txt", "*.md", "*.tex" },
-    callback = function()
-      vim.opt.spell = true
-      vim.opt.spelllang = "en,de"
-    end,
-  }
+	{ "BufRead", "BufNewFile" },
+	-- { pattern = { "*.txt", "*.md", "*.tex" }, command = [[setlocal spell<cr> setlocal spelllang=en,de<cr>]] }
+	{
+		pattern = { "*.txt", "*.md", "*.tex" },
+		callback = function()
+			vim.opt.spell = true
+			vim.opt.spelllang = "en,de"
+		end,
+	}
 )
 
-
 -- sync clipboards because I'm easily confused
-api.nvim_create_autocmd('TextYankPost', {
-  group    = bufcheck,
-  pattern  = '*',
-  callback = function() vim.fn.setreg('+', vim.fn.getreg('*'))
-end })
+api.nvim_create_autocmd("TextYankPost", {
+	group = bufcheck,
+	pattern = "*",
+	callback = function()
+		vim.fn.setreg("+", vim.fn.getreg("*"))
+	end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+api.nvim_create_autocmd({ "BufWritePre" }, {
+	group = augroup("auto_create_dir"),
+	callback = function(event)
+		if event.match:match("^%w%w+://") then
+			return
+		end
+		local file = vim.loop.fs_realpath(event.match) or event.match
+		vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+	end,
+})
--- home/.config/nvim/lua/lcavanzo/core/keymaps.lua
+++ home/.config/nvim/lua/lcavanzo/core/keymaps.lua
@@ -1,10 +1,6 @@
 vim.g.mapleader = " "
-
 -- Insert mode: Use 'jj' to quickly exit insert mode
 vim.keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with 'jj'" })
-
--- Normal mode: Clear search highlights when pressing '<leader>l'
-vim.keymap.set("n", "<leader>l", ":noh<return><esc>", { desc = "Clear search highlights with '<leader>l'" })
 
 -- Normal mode: Save the current file with '<leader>s'
 vim.keymap.set("n", "<leader>s", ":write<CR>", { desc = "Save the current file with '<leader>s'" })
@@ -13,36 +9,46 @@ vim.keymap.set("n", "<leader>s", ":write<CR>", { desc = "Save the current file w
 vim.keymap.set("n", "<leader>e", ":edit<CR>", { desc = "Refresh the currently open file with '<leader>e'" })
 
 -- Normal mode: Quit Vim with '<leader>q', or force quit with '<leader>Q'
-vim.keymap.set("n", "<leader>q", ":quit<CR>", { desc = "Quit Vim with '<leader>q', or force quit with '<leader>Q'" })
-vim.keymap.set("n", "<leader>Q", ":qa!<CR>", { desc = "Force quit Vim with '<leader>Q'" })
+vim.keymap.set("n", "<leader>qq", ":quit<CR>", { desc = "Quit Vim with '<leader>q', or force quit with '<leader>Q'" })
+vim.keymap.set("n", "<leader>QQ", ":qa!<CR>", { desc = "Force quit Vim with '<leader>Q'" })
+
+-- Normal mode: Manage Vim tabs
+vim.keymap.set("n", "<leader><tab><tab>", ":tabnew<CR>", { desc = "Create a new tab with '<leader>t'" })
+-- vim.keymap.set("n", "<leader>c", ":tabclose<CR>", { desc = "Close the current tab with '<leader>c'" })
+vim.keymap.set("n", "<leader>[", ":tabnext<CR>", { desc = "Switch to the next tab with '<leader>n'" })
+vim.keymap.set("n", "<leader>]", ":tabprevious<CR>", { desc = "Switch to the previous tab with '<leader>m'" })
 
 -- Normal mode: Uncomment the following lines to navigate between buffers
 vim.keymap.set("n", "<leader>[", ":bnext<CR>", { desc = "Switch to the next buffer with '<leader>['" })
 vim.keymap.set("n", "<leader>]", ":bprevious<CR>", { desc = "Switch to the previous buffer with '<leader>]'" })
 vim.keymap.set("n", "<leader>x", ":bdelete<CR>", { desc = "Delete the current buffer with '<leader>x'" })
 
--- Normal mode: Manage Vim tabs
-vim.keymap.set("n", "<leader>t", ":tabnew<CR>", { desc = "Create a new tab with '<leader>t'" })
-vim.keymap.set("n", "<leader>c", ":tabclose<CR>", { desc = "Close the current tab with '<leader>c'" })
-vim.keymap.set("n", "<leader>n", ":tabnext<CR>", { desc = "Switch to the next tab with '<leader>n'" })
-vim.keymap.set("n", "<leader>m", ":tabprevious<CR>", { desc = "Switch to the previous tab with '<leader>m'" })
-
 -- Normal mode: Toggle 'listchar' display with '<leader>d'
-vim.keymap.set("n", "<leader>d", ":set invlist<CR>", { desc = "Toggle 'listchar' display with '<leader>d'" })
+vim.keymap.set("n", "<leader>w", ":set invlist<CR>", { desc = "Toggle 'listchar' display with '<leader>d'" })
 
 -- Split window
-vim.keymap.set("n", "<leader>sh", ":split<Return><C-w>w", { silent = true })
-vim.keymap.set("n", "<leader>sv", ":vsplit<Return><C-w>w", { silent = true })
+vim.keymap.set("n", "<leader>s-", ":split<Return><C-w>w", { silent = true })
+vim.keymap.set("n", "<leader>s|", ":vsplit<Return><C-w>w", { silent = true })
 
--- Move window
--- vim.keymap.set("n", "<leader>", "<C-w>w")
-vim.keymap.set("", "<leader>hh", "<C-w>h")
-vim.keymap.set("", "<leader>kk", "<C-w>k")
-vim.keymap.set("", "<leader>jj", "<C-w>j")
-vim.keymap.set("", "<leader>ll", "<C-w>l")
+-- Move to window using the <ctrl> hjkl keys
+vim.keymap.set("", "<D-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+vim.keymap.set("", "<D-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+vim.keymap.set("", "<D-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+----- Not sure why only works in this way :(
+vim.keymap.set("", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
 
--- Resize window
-vim.keymap.set("n", "<C-w><left>", "<C-w><")
-vim.keymap.set("n", "<C-w><right>", "<C-w>>")
-vim.keymap.set("n", "<C-w><up>", "<C-w>+")
-vim.keymap.set("n", "<C-w><down>", "<C-w>-")
+-- Resize window using <ctrl> arrow keys
+vim.keymap.set("n", "<D-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+vim.keymap.set("n", "<D-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+vim.keymap.set("n", "<D-Left>", "<cmd>vertical resize +2<cr>", { desc = "Decrease window width" })
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize -2<cr>", { desc = "Increase window width" })
+
+-- Clear search with <esc>
+vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
+
+-- Add undo break-points
+vim.keymap.set("i", ",", ",<c-g>u")
+vim.keymap.set("i", ".", ".<c-g>u")
+vim.keymap.set("i", ";", ";<c-g>u")
+
+--- test keymap => :verbose nmap <D-Up>
