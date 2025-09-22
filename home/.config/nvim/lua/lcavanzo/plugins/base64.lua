8a54b5d8eafe714cdcfba518f3596222401b8cc6
luis cavanzo <cavanzo1996@gmail.com>
2025-09-21 18:39:37 -0600

    Delete home/.config/nvim/lua/lcavanzo/plugins/base64.lua

--- home/.config/nvim/lua/lcavanzo/plugins/base64.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"taybart/b64.nvim",
-	config = function()
-		local keymap = vim.keymap
-		keymap.set("v", "<leader>64e", "<cmd>B64Encode<CR>", { desc = "Base 64 Encode" }) -- save workspace session for current working directory
-		keymap.set("v", "<leader>64d", "<cmd>B64Decode<CR>", { desc = "Base 64 Decode" }) -- save workspace session for current working directory
-	end,
-}
