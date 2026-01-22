acda16504938e766ccd4c507b84a05dfbcc3ec14
lcavanzo <cavanzo1996@gmail.com>
2026-01-21 23:06:29 -0600

    update haung plugin nvim

--- home/.config/nvim/lua/plugins/haunt.lua
+++ home/.config/nvim/lua/plugins/haunt.lua
@@ -17,12 +17,29 @@ return {
       end,
       desc = "Delete Annotation",
     },
+    -- Remove all annotations in the workspace. Good for when you finish up a subtask
+    {
+      "<leader>hD",
+      function()
+        require("haunt.api").clear_all()
+      end,
+      desc = "Remove All the Annotations",
+    },
+    -- Toggle visibility of the current annotation
     {
       "<leader>ht",
       function()
         require("haunt.api").toggle_annotation()
       end,
       desc = "Toggle View",
+    },
+    -- Toggle visibility of the all annotations
+    {
+      "<leader>hT",
+      function()
+        require("haunt.api").toggle_all_lines()
+      end,
+      desc = "Toggle All Views",
     },
 
     -- Navigation (Jumping between ghosts)
@@ -41,6 +58,14 @@ return {
       desc = "Prev Haunt Bookmark",
     },
 
+    -- Search (Uses your Snacks.nvim picker)
+    {
+      "<leader>hl",
+      function()
+        require("haunt.picker").show()
+      end,
+      desc = "List All (Picker)",
+    },
     -- Search (Uses your Snacks.nvim picker)
     {
       "<leader>hl",
